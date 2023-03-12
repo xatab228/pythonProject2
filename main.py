d3b5de07b8c3d3ae04a65952509852f77d25d9ae
xatab228 <xatab228@gmail.com>
2023-03-12 16:37:00 +0300

    #main

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 
+# number
 
-print('hello')
+celoe = 123
+drobnoe = 123,456
+
+# string
+
+stroka = 'Dasha'
+
+# list - spisok
+
+arr = ['a',1, [1,2,3], 'bcd', celoe, stroka]
+print(arr, arr[0])
+
+# dictionary - slovar
+
+obj = {
+    'key': 'value',
+    1: 'as',
+    'cat': 'Vaska'
+}
+
+print(obj, obj[1])
