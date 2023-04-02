4010d8eecce068ae0dba91510478c1b28d1c4520
xatab228 <xatab228@gmail.com>
2023-04-02 12:54:18 +0300

    #15 tsaks

--- main.py
+++ main.py
@@ -1,24 +1,74 @@
 
-# number
 
-celoe = 123
-drobnoe = 123,456
+def Task34539():
+    P = list(range(22, 73))
+    Q = list(range(42, 102))
+    A = []
+    for x in range(0, 110):
+        # 22
+        #    False    True      True   True          False
+        if (not(not(x in A) and (x in P)) or (x in Q)) == False:
+            A.append(x)
+    return A;
 
-# string
 
-stroka = 'Dasha'
 
-# list - spisok
+def Del(n,m):
+    return n % m == 0
+def Task35904():
+    answer = 0
+    for A in range(1, 10000):
+        isComplete = True
+        for x in range(1, 10000):
+            if (Del(A, 40) and (Del(780, x) <= ((not(Del(A, x))) <= (not(Del(180, x)))))) == False:
+                isComplete = False
+                break;
+        if isComplete:
+            answer = A
+            break
 
-arr = ['a',1, [1,2,3], 'bcd', celoe, stroka]
-print(arr, arr[0])
+    return answer;
 
-# dictionary - slovar
 
-obj = {
-    'key': 'value',
-    1: 'as',
-    'cat': 'Vaska'
-}
+def Task35989():
+    answer = 0
+    for A in range(1, 10000):
+        isComplete = True
+        for x in range(1, 10000):
+            if ((x & 73 == 0) <= ((x & 28 != 0) <= (x & A != 0))) == False:
+                isComplete = False
+                break;
+        if isComplete:
+            answer = A
+            break
 
-print(obj, obj[1])
+    return answer;
+
+
+def Task19067():
+    answer = 0
+    for A in range(1, 1000):
+        isComplete = True
+        for x in range(1, 1000):
+            for y in range(1, 1000):
+                if ((x + 2 * y < A) or (y > x) or (x > 30)) == False:
+                    isComplete = False
+                    break;
+            if isComplete == False:
+                break
+        if isComplete:
+            answer = A
+            break
+    return answer;
+
+# answer 41 - 22 + 1 = 20
+# print(Task34539())
+
+# answer 120
+# print(Task35904())
+
+# answer 20
+# print(Task35989())
+
+# answer 91
+# print(Task19067())
