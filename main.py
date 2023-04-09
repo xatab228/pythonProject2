5197204f27e0317edde70197d9fea63f93ddc3a6
xatab228 <xatab228@gmail.com>
2023-04-09 17:09:55 +0300

    #16 tsaks

--- main.py
+++ main.py
@@ -1,74 +1,8 @@
 
 
-def Task34539():
-    P = list(range(22, 73))
-    Q = list(range(42, 102))
-    A = []
-    for x in range(0, 110):
-        # 22
-        #    False    True      True   True          False
-        if (not(not(x in A) and (x in P)) or (x in Q)) == False:
-            A.append(x)
-    return A;
+def F(n):
+    if n <= 2:
+        return 2;
+    return 3 * F(n-1) - F(n-2);
 
-
-
-def Del(n,m):
-    return n % m == 0
-def Task35904():
-    answer = 0
-    for A in range(1, 10000):
-        isComplete = True
-        for x in range(1, 10000):
-            if (Del(A, 40) and (Del(780, x) <= ((not(Del(A, x))) <= (not(Del(180, x)))))) == False:
-                isComplete = False
-                break;
-        if isComplete:
-            answer = A
-            break
-
-    return answer;
-
-
-def Task35989():
-    answer = 0
-    for A in range(1, 10000):
-        isComplete = True
-        for x in range(1, 10000):
-            if ((x & 73 == 0) <= ((x & 28 != 0) <= (x & A != 0))) == False:
-                isComplete = False
-                break;
-        if isComplete:
-            answer = A
-            break
-
-    return answer;
-
-
-def Task19067():
-    answer = 0
-    for A in range(1, 1000):
-        isComplete = True
-        for x in range(1, 1000):
-            for y in range(1, 1000):
-                if ((x + 2 * y < A) or (y > x) or (x > 30)) == False:
-                    isComplete = False
-                    break;
-            if isComplete == False:
-                break
-        if isComplete:
-            answer = A
-            break
-    return answer;
-
-# answer 41 - 22 + 1 = 20
-# print(Task34539())
-
-# answer 120
-# print(Task35904())
-
-# answer 20
-# print(Task35989())
-
-# answer 91
-# print(Task19067())
+print(F(6))
