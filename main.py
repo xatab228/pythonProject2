6d6cf2ddb68533afd43b0728bc80ef2b48655ae6
xatab228 <xatab228@gmail.com>
2023-04-28 20:06:15 +0300

    #19 tsaks

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ def gameData(stones, stepsCount):
         else:
             stepData = stepsData[step - 1]
             newStep = []
-            for stoneIndex in range(len(stepData)):
-                newStep.append(stepData[stoneIndex] + 1)
-                newStep.append(stepData[stoneIndex] * 2)
+            for stone in stepData:
+                newStep.append(stone + 1)
+                newStep.append(stone * 2)
             stepsData.append(newStep)
     return stepsData
 
 
 def StoneTask():
-    defaultStonesCount = 2;
-    defaultStepsCount = 2;
-    game = gameData(defaultStonesCount, defaultStepsCount);
+    defaultStonesCount = 31 #S
+    defaultStepsCount = 4
+    game = gameData(defaultStonesCount, defaultStepsCount)
 
-    #ONLY PRINT
+    #ONLY FOR PRINT
     stepsCount = 1
     countSpace = 32
     print('S' + int(countSpace + 6) * ' ' + str(defaultStonesCount))
