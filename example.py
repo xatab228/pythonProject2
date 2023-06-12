22eddb1e618558f286a426f2e5eef11cfaa24fff
xatab228 <xatab228@gmail.com>
2023-06-12 13:50:56 +0300

    #26-27 tsaks

--- example.py
+++ example.py
@@ -14,9 +14,8 @@ def Task26_1():
             values.append(int(string[i]))
 
     values = sorted(values)
-    print(values)
-    maxValueSumm = 0;
-    maxValue = 0;
+    maxValueSumm = 0
+    maxValue = 0
     counter = 0
     for value in values:
         if (maxValueSumm + int(value)) <= int(params['max']):
@@ -33,26 +32,28 @@ def Task26_1():
             counter += 1
     print(counter, maxValue, maxValueSumm)
 
-    def Task27_1():
-        file = open('../files/27_1_B.txt')
-        string = file.readlines()
-        N = 0
-        values = []
-        for i in range(len(string)):
-            if i == 0:
-                N = string[i]
-            else:
-                values.append(int(string[i]))
+def Task27_1():
+    file = open('../files/27_1_A.txt')
+    string = file.readlines()
+    N = 0
+    values = []
+    for i in range(len(string)):
+        if i == 0:
+            N = string[i]
+        else:
+            values.append(int(string[i]))
 
-        maxSumm = 0
-        firstNum = 0
-        secondNum = 0
-        for i in range(len(values) - 1):
-            for j in range(i + 1, len(values)):
-                summ = values[i] + values[j]
-                if (summ > maxSumm) and (summ % 120 == 0) and (values[i] > values[j]):
-                    maxSumm = summ
-                    firstNum = values[i]
-                    secondNum = values[j]
+    maxSumm = 0
+    firstNum = 0
+    secondNum = 0
+    for i in range(len(values) - 1):
+        for j in range(i + 1, len(values)):
+            summ = values[i] + values[j]
+            if (summ > maxSumm) and (summ % 120 == 0) and (values[i] > values[j]):
+                maxSumm = summ
+                firstNum = values[i]
+                secondNum = values[j]
 
-        print(firstNum, secondNum, values)
+    print(firstNum, secondNum, values)
+
+Task27_1()
